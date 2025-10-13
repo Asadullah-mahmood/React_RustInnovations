47a61dd60d8ea4c6fe40cab0912012454ae885c2
hafiz zubair <hafizzubair7867@gmail.com>
2025-10-13 16:21:02 +0000

    Please add this logo in main and footer where you used main icon

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,18 +6,11 @@ export function Logo() {
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        fill="currentColor"
         className="h-7 w-7 text-primary"
         aria-hidden="true"
       >
-        <path d="M12 8v4l3 3" />
-        <path d="M12 21a9 9 0 1 0-9-9" />
-        <path d="M22 22 18 18" />
-
+        <path d="M11.996 1.875c.45 0 .81.39.81.87v4.61h4.61c.48 0 .87.36.87.81v4.99c0 .45-.39.81-.87.81h-4.61v4.61c0 .48-.36.87-.81.87h-4.99c-.45 0-.81-.39-.81-.87v-4.61H1.586c-.48 0-.87-.36-.87-.81v-4.99c0-.45.39-.81.87-.81h4.61v-4.61c0-.48.36-.87.81-.87h4.99zM8.006 8.165H3.396v3.1h4.61v-3.1zm0 4.78h4.79v4.61h-4.79v-4.61zm6.57 0h4.61v-3.1h-4.61v3.1z"></path>
       </svg>
       <div className="flex flex-col">
         <span className="font-headline text-2xl font-bold text-foreground">
