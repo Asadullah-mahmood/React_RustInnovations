dcf94d84c35c1e263acd33eb241d5e64c652c4a0
hafiz zubair <hafizzubair7867@gmail.com>
2026-02-14 22:26:40 +0000

    remove this main logo and and use /assets/logo.svg without animation

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import Link from 'next/link';
+import Image from 'next/image';
 import { cn } from '@/lib/utils';
-import { motion } from 'framer-motion';
 
 export function Logo({ iconOnly = false }: { iconOnly?: boolean }) {
   return (
@@ -11,34 +11,14 @@ export function Logo({ iconOnly = false }: { iconOnly?: boolean }) {
       className="flex flex-row items-center gap-1.5 md:gap-1.5 ml-2 md:ml-4"
       aria-label="Rust Innovations Home"
     >
-      <motion.svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 100 100"
+      <Image
+        src="/assets/logo.svg"
+        alt="Rust Innovations Logo"
+        width={64}
+        height={64}
         className="h-16 w-16"
-      >
-        <motion.path
-          d="M25 80 V20 H50 C75 20, 75 50, 50 50 H35 L65 80"
-          stroke="hsl(var(--primary))"
-          strokeWidth="10"
-          fill="transparent"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          initial={{ pathLength: 0, opacity: 0 }}
-          animate={{ pathLength: 1, opacity: 1 }}
-          transition={{
-            pathLength: {
-              delay: 0.2,
-              type: 'tween',
-              ease: 'easeInOut',
-              duration: 2,
-              repeat: Infinity,
-              repeatType: 'loop',
-              repeatDelay: 1,
-            },
-            opacity: { delay: 0.2, duration: 0.01, repeat: Infinity },
-          }}
-        />
-      </motion.svg>
+        priority
+      />
       <div
         className={cn(
           'flex flex-col justify-center overflow-hidden transition-all duration-300',
